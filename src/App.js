b069700a8d1ac148ad8cb630ec8e07078f862641
MTTParry <michaela.t.t.parry@gmail.com>
2022-08-17 10:56:54 -0700

    added x2 + x5 buttons + functions

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 function App() {
   const [value, setValue] = useState(0);
-
+  //base functions
   function lowerValue() {
     setValue(value - 1);
   }
@@ -12,11 +12,25 @@ function App() {
     setValue(value + 1);
   }
 
+  //multiply
+  function multiply5() {
+    setValue(value * 5);
+  }
+
+  function double() {
+    setValue(value * 2);
+  }
+
   return (
     <div className="App">
       <button onClick={lowerValue}>-</button>
       <span>{value}</span>
       <button onClick={upValue}>+</button>
+      <br />
+      <hr/>
+      <br />
+      <button onClick={double}>x2</button>
+      <button onClick={multiply5}>x5</button>
     </div>
   );
 }
